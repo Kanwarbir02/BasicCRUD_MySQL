62544989c12603101fa1865d474ba4b7946fdbf3
Kanwarbir02 <kanwarbir2002@gmail.com>
2022-01-17 22:00:31 +0530

    states and debugging

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,42 @@
 import "./bootstrap.css"
 import './App.css';
 import {Form, Button} from "react-bootstrap"
+import { useState } from "react";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [location, setLocation] = useState("");
+  const [id, setId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Succesfully Submitted")
+  }
+
   return (
     <div className="App">
-      <Form className='infoform'>
+      <Form className='infoform' onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail" className='formGroup'>
-            <Form.Label>Email address</Form.Label><br/>
-            <Form.Control type="email" placeholder="Enter email" className='formInput'/>
+            <Form.Label>Name</Form.Label><br/>
+            <Form.Control type="text" placeholder="Enter Name" className='formInput' onChange={(e) => {setName(e.target.value)}}/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail" className='formGroup'>
+            <Form.Label>Age</Form.Label><br/>
+            <Form.Control type="text" placeholder="Enter Age" className='formInput'  onChange={(e) => {setAge(e.target.value)}}/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail" className='formGroup'>
+            <Form.Label>Location</Form.Label><br/>
+            <Form.Control type="text" placeholder="Enter your location" className='formInput'  onChange={(e) => {setLocation(e.target.value)}}/>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword" className='formGroup'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" className='formInput'/>
+            <Form.Label>ID_Number</Form.Label>
+            <Form.Control type="text" placeholder="Your ID Number" className='formInput'  onChange={(e) => {setId(e.target.value)}}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox" className='formGroup'>
             <Form.Check type="checkbox" label="Check me out" />
@@ -22,6 +45,9 @@ function App() {
             Submit
           </Button>
         </Form>
+
+
+        <h2>{name + " " + age + " " + location + " " + id}</h2>
     </div>
   );
 }
